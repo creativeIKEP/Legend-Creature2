2f714651fcd2556ebd4285d954f1221cdf012596
creativeIKEP <syunya.i.1021@gmail.com>
2018-07-28 01:06:55 +0900

    回転退避実装

---
+++ SmartPhoneUICtrl.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SmartPhoneUICtrl : MonoBehaviour {
+    PlayerMove playerMove;
+    PlayerAttack playerAttack;
+    InputAttack.InputAttackCheacck inputAttackCheacck;
+    Vector2 prevPosition;
+    Vector2 delta = Vector2.zero;
+    bool moved = false;
+    int touchCount = 0;
+
+	// Use this for initialization
+	void Start () {
+        playerMove = FindObjectOfType<PlayerMove>();
+        playerAttack = FindObjectOfType<PlayerAttack>();
+        inputAttackCheacck = FindObjectOfType<InputAttack.InputAttackCheacck>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!moved) { delta = Vector2.zero; }
+	}
+
+    public bool Clicked()
+    {
+        if (!moved && Input.GetMouseButtonUp(0))
+            return true;
+        else
+            return false;
+    }
+
+    // スライド時のカーソルの移動量.
+    public Vector2 GetDeltaPosition()
+    {
+        return delta;
+    }
+
+    // スライド中か.
+    public bool Moved()
+    {
+        return moved;
+    }
+
+    public Vector2 GetCursorPosition()
+    {
+        return Input.mousePosition;
+    }
+
+
+    public void JoyStickEnter(){
+        touchCount++;
+    }
+    public void JoyStickExit(){
+        touchCount--;
+    }
+
+    //public void ForwardButton(){
+    //    playerMove.ForwardMove();
+    //}
+    //public void BackButton()
+    //{
+    //    playerMove.BackMove();
+    //}
+    //public void LeftButton()
+    //{
+    //    playerMove.LeftMove();
+    //}
+    //public void RightButton()
+    //{
+    //    playerMove.RightMove();
+    //}
+
+    //public void ForwardButtonUp()
+    //{
+    //    playerMove.ForwardMoveStop();
+    //}
+    //public void BackButtonUp()
+    //{
+    //    playerMove.BackMoveStop();
+    //}
+    //public void LeftButtonUp()
+    //{
+    //    playerMove.LeftMoveStop();
+    //}
+    //public void RightButtonUp()
+    //{
+    //    playerMove.RightMoveStop();
+    //}
+    public void DashButton(){
+        playerMove.DushMove();
+        touchCount++;
+    }
+    public void DashMoveStop(){
+        playerMove.DushMoveStop();
+        touchCount--;
+    }
+
+    public void AttackButton(){
+        playerAttack.Attack();
+        StartCoroutine(inputAttackCheacck.Attack());
+    }
+
+    public void ScrollButtonDown(){
+        if (Input.touchCount == touchCount + 2)
+        {
+            
+        }
+        else if (Input.touchCount == touchCount + 1)
+        {
+            prevPosition = GetCursorPosition();
+        }
+    }
+    public void ScrollButton()
+    {
+        delta = GetCursorPosition() - prevPosition;
+        prevPosition = GetCursorPosition();
+        moved = true;
+    }
+    public void ScrollButtonUp()
+    {
+        prevPosition = GetCursorPosition();
+        moved = false;
+    }
+
+    public void JumpButton(){
+        playerMove.Jump();
+    }
+}
